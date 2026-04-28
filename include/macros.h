445afcd38bb6cbbf113aaa843fcdb6ccd8cded27
lammmab <liamlam8282@gmail.com>
2026-04-28 15:58:42 -0700

    Siphoning it all day

--- include/macros.h
+++ include/macros.h
@@ -14,4 +14,12 @@
   #define IS_REG_FILE(st) S_ISREG((st).st_mode)
 #endif
 
+#if defined(_MSC_VER)
+    #include <string.h>
+    #define strcasecmp _stricmp
+    #define strncasecmp _strnicmp
+#else
+    #include <strings.h>
+#endif
+
 #endif
--- src/dol/gc_dol.c
+++ src/dol/gc_dol.c
@@ -9,7 +9,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 #include <sys/stat.h>
 #include <errno.h>
 #include "macros.h"
